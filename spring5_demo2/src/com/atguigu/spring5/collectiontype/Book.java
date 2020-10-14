90beb82871bf766772532c1c5080677e5b1d8a7a
forrestlmj <18810276930@163.com>
2020-10-14 16:19:30 +0800

    util

--- spring5_demo2/src/com/atguigu/spring5/collectiontype/Book.java
+++ spring5_demo2/src/com/atguigu/spring5/collectiontype/Book.java
@@ -12,12 +12,12 @@ import java.util.List;
 public class Book {
     private List<String> list;
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                "list=" + list +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Book{" +
+//                "list=" + list +
+//                '}';
+//    }
 
     public void setList(List<String> list) {
         this.list = list;
--- spring5_demo2/src/com/atguigu/spring5/testdemo/TestSpring5Demo1.java
+++ spring5_demo2/src/com/atguigu/spring5/testdemo/TestSpring5Demo1.java
@@ -1,10 +1,13 @@
 package com.atguigu.spring5.testdemo;
 
+import com.atguigu.spring5.collectiontype.Book;
 import com.atguigu.spring5.collectiontype.Stu;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * @author ：xxx
@@ -20,5 +23,14 @@ public class TestSpring5Demo1 {
                 new ClassPathXmlApplicationContext("bean1.xml");
         Stu stu = applicationContext.getBean("stu",Stu.class);
         System.out.println(stu);
+    }
+    @Test
+    public void testBean2(){
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("bean2.xml");
+        Book book1 = applicationContext.getBean("book",Book.class);
+        Book book2 = applicationContext.getBean("book",Book.class);
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
