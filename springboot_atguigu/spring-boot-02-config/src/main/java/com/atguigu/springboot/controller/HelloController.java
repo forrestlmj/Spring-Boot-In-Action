9df84116343de4e04cae6735d602a1e157c2f9d8
forrestlmj <18810276930@163.com>
2020-11-03 09:36:09 +0800

    使用Value+Componet做成注解，用于将配置文件中内容加载到 容器中

--- springboot_atguigu/spring-boot-02-config/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ springboot_atguigu/spring-boot-02-config/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.atguigu.springboot.controller;
 
 import com.atguigu.springboot.bean.Person;
+import com.atguigu.springboot.bean.TestValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author ：xxx
@@ -14,12 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+
+    @Resource
+    private TestValue testValue;
     @Value("${person.last-name}")
     private String lastName;
     @Autowired
     private Person person;
     @GetMapping("sayHello")
     public String hello(){
+
         return "hello:"+person;
+    }
+    @GetMapping("testValue")
+    public String testValue(){
+
+        return "testValue:"+testValue.toString();
     }
 }
