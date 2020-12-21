16024e192b2390b20189c4b10ca9c7ff5e7acc89
forrestlmj <18810276930@163.com>
2020-12-21 11:45:20 +0800

    增加日志

--- springboot_atguigu/spring-boot-06-data-jdbc/src/main/java/com/atguigu/controller/HelloController.java
+++ springboot_atguigu/spring-boot-06-data-jdbc/src/main/java/com/atguigu/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.atguigu.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +20,13 @@ import java.util.Map;
 
 @RestController
 public class HelloController {
-
+    Logger logger = LoggerFactory.getLogger(HelloController.class.getName());
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @GetMapping("/query")
     public List<Map<String,Object>> map(){
+        logger.info("访问");
         List<Map<String,Object>> list = jdbcTemplate.queryForList("select * FROM department");
         return list;
     }
